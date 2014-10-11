cca09cfaee3e5e1a0587263fc0bc61b612fa602f
rafaelfavretti <rafael.favretti@gmail.com>
2014-10-11 17:00:52 -0300

    Primeiro commit
    Funcionalidade login por Facebook;
    Funcionalidade logoff aplicação;
    Redirecionamento para tela de busca de produto;
    
    Importante!!!
    
    Necessário download do Facebook SDK, realizar import do mesmo conforme
    instruções fornecidas na página
    https://developers.facebook.com/docs/android/login-with-facebook/v2.1

---
+++ Ofertame/src/com/secondfloor/ofertame/activity/MainActivity.java
@@ -0,0 +1,100 @@
+package com.secondfloor.ofertame.activity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.model.GraphUser;
+
+public class MainActivity extends ActionBarActivity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		//Start Facebook Login
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+
+			// callback when session changes state
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				if (session.isOpened()) {
+					Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+								// callback after Graph API response with user
+								// object
+								@Override
+								public void onCompleted(GraphUser user,
+										Response response) {
+									if (user != null) {
+										  TextView welcome = (TextView) findViewById(R.id.entrada);
+										  welcome.setText("Seja Bem-Vindo " + user.getName() + "!");
+										  
+										}
+								}
+							}).executeAsync();
+					
+					//Seto um timer para redirecionamento
+					int timeout = 5000;
+					Timer timer = new Timer();
+					timer.schedule(new TimerTask() {
+
+					    @Override
+					    public void run() {
+					        finish();
+					        //Redireciono para a Activity de busca de produto
+					        Intent intent = new Intent(MainActivity.this, BuscaProdutoActivity.class);
+							startActivity(intent);
+					    }
+					}, timeout);
+					
+				}
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		} else if (id == R.id.deslogar){
+			deslogar();
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode,
+				resultCode, data);
+	}
+	
+	
+	public void deslogar(){
+		Session.getActiveSession().closeAndClearTokenInformation();
+	}
+}
